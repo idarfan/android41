3546c34155cf8072c6f5b63995d1716b3c380d77
idarfan <mr.idarfan@gmail.com>
2012-07-17 20:13:25 +0800

    first commit

---
+++ Ch05app2me/gen/kojen/ch05app2me/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kojen.ch05app2me;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ PA4AD_Ch02_Hello_World/gen/com/paad/pa4ad_ch02_hello_world/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.paad.pa4ad_ch02_hello_world;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
